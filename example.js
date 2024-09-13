9301bf76d8c65edc66e504386a4c04c29cad39b7
Umut <umut.mkt@gmail.com>
2024-09-13 10:22:02 +0300

    example.js update

--- example.js
+++ example.js
@@ -1,5 +1,14 @@
 const setupLogger = require("dera-logger");
 
+// levels
+//     error: 0,
+//     warn: 1,
+//     info: 2,
+//     http: 3,
+//     verbose: 4,
+//     debug: 5,
+//     silly: 6
+
 const options = {
   logDirectory: "logs",
   timestampFormat: "HH:mm:ss",
@@ -13,10 +22,12 @@ const options = {
     { filename: "error", level: "warn", source: "backend" },
     { filename: "combined", level: "silly", source: "frontend" },
     { filename: "error", level: "warn", source: "frontend" },
+    { filename: "combined", level: "silly" }, // source is optional
+    { filename: "error", level: "warn" },
   ],
 };
 
 const logger = setupLogger(options);
 
 logger.error("An error occurred", "backend");
-logger.info("A new request has been received", "frontend");
+logger.info("A new request has been received");
